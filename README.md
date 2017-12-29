c79e95b3c9c94bebdd45cb7e1ffab256c3131cd5
Efrain "Rain" Rivas <rainrivas@gmail.com>
2017-12-28 23:59:40 -0800

    Npm rework (#1)
    * Base structure development. Pseudo code notes.
    
    * Source formatting. Add conditional check for null argument
    
    * Add installation instructions
    
    * Add conditional checks for argument variables
    
    * Update shebang for mac compat
    
    * Setting up basic node logic for npm
    
    * Fixing output by removing the needless counter
    
    * 0.0.2
    
    * Fixing read me with npm instructions
    
    * updating to 0.1.0 as beta build is now ready for basic use

--- README.md
+++ README.md
@@ -2,7 +2,18 @@
 > Get hip or die trying
 
 ### Installation
+`slng` is on npm! Check out out [here](https://www.npmjs.com/package/slng)
 
+```
+npm install -g slng
+```
+
+#### Usage
+Once installed, you should be able to call with:
+
+```
+slng <search string>
+```
 
 ### License
 [MIT](https://github.com/jwu910/slng/blob/master/LICENSE)
---
+++ index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node --harmony
+
+var program = require('commander'),
+  request = require('request');
+
+program
+  .arguments('<slng>')
+  .action((slng) => {
+    var options = {
+      method: 'GET',
+      url: 'http://api.urbandictionary.com/v0/define',
+      qs: {
+        term: slng
+      },
+      headers: {
+        'Cache-Control': 'no-cache',
+        Accept: 'application/json'
+      }
+    }
+
+    request(options, function (err, res, body) {
+      if (err) throw new Error(err);
+      var trimRes;
+      var results = JSON.parse(body).list;
+      if (results.length > 3) {
+        trimRes = results.slice(0, 3);
+      } else {
+        trimRes = results;
+      }
+      trimRes.forEach((result) => {
+        if (typeof (result.definition) !== undefined) {
+          console.log("=======================\n" + result.definition);
+        }
+      });
+    });
+  })
+  .parse(process.argv);
