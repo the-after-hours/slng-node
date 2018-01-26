9c766d4d91609ceabd275f06ec9bc56bde3ab295
Duc Ngo <duc.ngos.it.all@gmail.com>
2018-01-25 17:55:03 -0800

    Remove the optional result action and add the number of results to display flag

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const consoleWidth = () => {
 
 program
   .version('0.2.0')
-  .arguments('<phrase> [number]')
-  .action((slng, numberOfResults) => {
+  .arguments('<phrase>')
+  .option('-r, --results', 'Specify the number results to display', '3' )
+  .action((slng) => {
     var options = {
       method: 'GET',
       url: 'http://api.urbandictionary.com/v0/define',
@@ -24,39 +25,40 @@ program
       }
     }
 
-    var num = numberOfResults;
-
-    if(numberOfResults === undefined) {
-      num = 3;
-    }
-
     request(options, function (err, res, body) {
       if (err) throw new Error(err);
       var trimRes;
       var results = JSON.parse(body).list;
+      var resultsToDisplay = 3;
 
       console.log('='.repeat(consoleWidth()));
+
+      if(typeof process.argv[4] !== 'undefined'){
+        if(typeof Number(process.argv[4]) === 'number'){
+          resultsToDisplay = Number(process.argv[4]);
+        }
+      }
 
       if (results.length === 0) {
         console.log(chalk.red('No results were found, please try another phrase'));
       } else {
-        if (results.length > num) {
-          trimRes = results.slice(0, num);
+        if (results.length > resultsToDisplay) {
+          trimRes = results.slice(0, resultsToDisplay);
         } else {
           trimRes = results;
         }
         trimRes.forEach((result) => {
           if (typeof (result.definition) !== undefined) {
-            console.log('Word: ' + chalk.bold(result.word));
-            console.log('Definition: ' + chalk.hex('#ffb3ff')(result.definition));
-            console.log('Score: ' + (result.thumbs_up - result.thumbs_down));
+            console.log(chalk.hex('#ffb3ff').bold('Word: ') + chalk.hex('#ffb3ff').bold(result.word));
+            console.log(chalk.hex('#ffb3ff').bold('Definition: ') + result.definition);
+            console.log(chalk.hex('#ffb3ff').bold('Score: ') + (result.thumbs_up - result.thumbs_down));
             console.log(chalk.bold.green('Ayys: ') + result.thumbs_up + ' | ' + chalk.bold.red('Nayys: ') + result.thumbs_down);
             console.log('='.repeat(consoleWidth()));
           }
         });
       }
     });
-  });
+  })
 
 program.on('--help', function () {
   console.log('');
