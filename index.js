83bf8fd0b4683fe0985e979f39b51abad167f5d4
Joshua Wu <joshua.wu.0910@gmail.com>
2018-02-04 00:57:31 -0800

    Add api features to console logs for extra results information (#23)
    * Add api features to console logs for extra results information
    
    * Slng output color (#27)
    
    * Adding a shade of magenta to the font color and making it bold.
    
    * remove the option for additional results to display
    
    * Change magenta variable to FONT_COLOR and fix the corresponding variables with it.
    Removed the parenthesis around results.word
    
    * setting word color to cyan since none of the magenta's would load on windows

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const chalk = require('chalk');
 const program = require('commander');
 const request = require('request');
 
+const consoleWidth = () => {
+  return parseInt(process.stdout.columns)
+}
+
 program
   .version('0.2.0')
   .arguments('<phrase>')
@@ -24,17 +28,25 @@ program
       if (err) throw new Error(err);
       var trimRes;
       var results = JSON.parse(body).list;
+      var resultsToDisplay = 3;
+
+      console.log('='.repeat(consoleWidth()));
+
       if (results.length === 0) {
         console.log(chalk.red('No results were found, please try another phrase'));
       } else {
-        if (results.length > 3) {
-          trimRes = results.slice(0, 3);
+        if (results.length > resultsToDisplay) {
+          trimRes = results.slice(0, resultsToDisplay);
         } else {
           trimRes = results;
         }
         trimRes.forEach((result) => {
           if (typeof (result.definition) !== undefined) {
-            console.log(chalk.bold.cyan('=======================\n') + result.definition);
+            console.log(chalk.bold.cyan('Word: ') + result.word);
+            console.log(chalk.bold.cyan('Definition: ') + result.definition);
+            console.log(chalk.bold.cyan('Score: ') + (result.thumbs_up - result.thumbs_down));
+            console.log(chalk.bold.green('Ayys: ') + result.thumbs_up + ' | ' + chalk.bold.red('Nayys: ') + result.thumbs_down);
+            console.log('='.repeat(consoleWidth()));
           }
         });
       }
