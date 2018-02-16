581daca20350047a9036d536039cf56b443bb347
Efrain 'Rain' Rivas <rainrivas@gmail.com>
2018-02-15 17:10:58 -0800

    Getting the random word flag up and running for #26

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ const consoleWidth = () => {
 
 program
   .version('0.3.0')
+  .option('-R, --random', 'Display random words (up to 3), cannot be used when passing a phrase')
   .arguments('<phrase>')
   .action((slng) => {
-    var options = {
+    let options = {
       method: 'GET',
       url: 'http://api.urbandictionary.com/v0/define',
       qs: {
@@ -62,3 +63,43 @@ program.on('--help', function () {
 });
 
 program.parse(process.argv);
+
+if(program.random && process.argv.slice(2).length===1){
+  let options = {
+      method: 'GET',
+      url: 'http://api.urbandictionary.com/v0/random',
+      headers: {
+        'Cache-Control': 'no-cache',
+        Accept: 'application/json'
+      }
+    }
+
+    request(options, function (err, res, body) {
+      if (err) throw new Error(err);
+      var trimRes;
+      var results = JSON.parse(body).list;
+      var resultsToDisplay = 3;
+
+      console.log('='.repeat(consoleWidth()));
+
+      if (results.length === 0) {
+        console.log(chalk.red('No results were found, please try another phrase'));
+      } else {
+        if (results.length > resultsToDisplay) {
+          trimRes = results.slice(0, resultsToDisplay);
+        } else {
+          trimRes = results;
+        }
+        trimRes.forEach((result) => {
+          if (typeof (result.definition) !== undefined) {
+            console.log(chalk.bold.cyan('Word: ') + result.word);
+            console.log(chalk.bold.cyan('Definition: ') + result.definition);
+            console.log(chalk.bold.cyan('Score: ') + (result.thumbs_up - result.thumbs_down));
+            console.log(chalk.bold.green('Ayys: ') + result.thumbs_up + ' | ' + chalk.bold.red('Nayys: ') + result.thumbs_down);
+            console.log('='.repeat(consoleWidth()));
+          }
+        });
+      }
+    });
+
+}
